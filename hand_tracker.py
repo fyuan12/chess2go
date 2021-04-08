841aa8282f2f2452ac2aa45912b3517e4755a61e
Franklin Yuan <yuan.franklin@gmail.com>
2021-04-08 16:21:54 -0600

    added pinch detection sample code

--- hand_tracker.py
+++ hand_tracker.py
@@ -9,8 +9,10 @@ pip install mediapipe
 import cv2
 import mediapipe as mp
 import time
+import math
+import queue
 
-class HandDetector():
+class HandTracker():
     def __init__(self, mode=False, max_hands=2, min_detect_confidence=0.5, min_track_confidence=0.5):
         self.mode = mode
         self.max_hands = max_hands
@@ -20,11 +22,14 @@ class HandDetector():
         self.mp_hands = mp.solutions.hands
         self.hands = self.mp_hands.Hands(self.mode, self.max_hands, self.min_detect_confidence, self.min_track_confidence)
         self.mp_drawing = mp.solutions.drawing_utils
+        
+        self.pinch_state = False
+        self.false_counter = 0 # used to avoid false negatives
 
-    def find_hands(self, img, draw=True):
-        # Flip the image horizontally for a later selfie-view display, and convert
-        # the BGR image to RGB.
-        img = cv2.flip(img, 1)
+    def find_hands(self, img, selfie=True, draw=True):
+        # Selfie: Flip the image horizontally for a later selfie-view displayconvert
+        if selfie:
+            img = cv2.flip(img, 1)
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         # To improve performance, optionally mark the image as not writeable to
         # pass by reference.
@@ -35,13 +40,14 @@ class HandDetector():
         # img_rgb.flags.writeable = True
         # img_rgb = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR)
         if self.results.multi_hand_landmarks:
-            for hand_landmarks in self.results.multi_hand_landmarks:
-                self.mp_drawing.draw_landmarks(
-                    img, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
+            if draw:
+                for hand_landmarks in self.results.multi_hand_landmarks:
+                    self.mp_drawing.draw_landmarks(
+                        img, hand_landmarks, self.mp_hands.HAND_CONNECTIONS)
             return True, img
         return False, img
 
-    def get_coordinates(self, img, hand_no=0):
+    def _get_coordinates(self, img, hand_no=0):
         lm_list = []
 
         if self.results.multi_hand_landmarks:
@@ -49,9 +55,32 @@ class HandDetector():
             for i, landmark in enumerate(hand_landmarks.landmark):
                 h, w, c = img.shape
                 cx, cy = int(landmark.x * w), int(landmark.y * h)
-                print(i, cx, cy)
                 lm_list.append([i, cx, cy])
         return lm_list
+
+    def get_pinch(self, img, max_dist, draw=True):
+        lmList = self._get_coordinates(img)
+        dist = 0.0
+        if len(lmList) != 0:
+            x1, y1 = lmList[4][1], lmList[4][2]
+            x2, y2 = lmList[8][1], lmList[8][2]
+            cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+            dist = math.hypot(x2 - x1, y2 - y1) # pixel distance between thumb and index
+            
+            # if it's a pinch draw the pinch
+            if dist < max_dist:
+                cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+                cv2.circle(img, (x2, y2), 15, (255, 0, 255), cv2.FILLED)
+                cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+                cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+                self.false_counter = 0
+                self.pinch_state = True
+            else:
+                self.false_counter += 1
+                # check if the pinch state has been false for at least 5 frames in a row
+                if self.false_counter >= 5:
+                    self.pinch_state = False
+            return self.pinch_state, (cx, cy)
 
 def main():
     p_time = 0
@@ -63,8 +92,6 @@ def main():
         found, img = detector.find_hands(img)
         if found:
             lmList = detector.get_coordinates(img)
-            # if len(lmList) != 0:
-            #     print(lmList[4])
  
         c_time = time.time()
         fps = 1/(c_time - p_time)
---
+++ pinch_detection_sample_code.py
@@ -0,0 +1,48 @@
+"""
+This is a sample code to find the pinch point in a video stream
+
+The HandTracker class has two main methods:
+- find_hands() returns 1) whether a hand is found and 2) the flipped image
+- get_pinch(): returns 1) whether a pinch is detected and 2) the xy coordinates of the pinch point
+"""
+
+import cv2
+import time
+import numpy as np
+import queue
+from hand_tracker import HandTracker
+
+MAX_PINCH_DIST = 100
+
+def main():
+    p_time = 0
+    c_time = 0
+    cap = cv2.VideoCapture(1)
+
+    tracker = HandTracker(min_detect_confidence=0.7)
+
+    while True:
+        ret, img = cap.read()
+
+        found, img = tracker.find_hands(img, selfie=True, draw=False)
+        # if at least one hand is found
+        if found:
+            detected, pinch_pt = tracker.get_pinch(img, max_dist=MAX_PINCH_DIST, draw=True)
+            if detected:
+                print('A pinch is detected:', pinch_pt) # the x,y coordinate of the pinch point
+            else:
+                print('A pinch is not detected.')
+
+        # calculate and output fps
+        c_time = time.time()
+        fps = 1/(c_time - p_time)
+        p_time = c_time
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                    (255, 0, 255), 3)
+ 
+        cv2.imshow('MediaPipe Hands', img)
+        if cv2.waitKey(5) & 0xFF == ord('q'):
+            break
+ 
+if __name__ == "__main__":
+    main()
