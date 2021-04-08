3404628b807e842d6968184e71049ccc4efec8df
sequoiap <sequoiap4@gmail.com>
2021-04-08 09:34:19 -0600

    Added square names

--- pychess.py
+++ pychess.py
@@ -34,6 +34,17 @@ Board positions are named as follows:
 3   o   o   o   o
 2 o   o   o   o  
 1   o   o   o   o
+
+Squares are also numbered like so:
+   A B C D E F G  H
+8|      ...    62 63
+7| 
+6|        .       .
+5|       .        .
+4|      .         .
+3| 
+2| 
+1| 1 2 3 4 5 6 7  8
 """
 
 print(chess.STARTING_BOARD_FEN)
@@ -45,4 +56,7 @@ print("Can white move first?", True if chess.Move.from_uci('a2a4') else False)
 print("Is moving D7 to D5 a legal move?", True if chess.Move.from_uci('d7d5') in board.legal_moves else False)
 print("Let's advance a pawn. Pushing white pawn D2 to D4:", board.push_uci('d2d4'))
 print(board)
-print("What are all the legal moves now?\n", [move for move in board.legal_moves])
+moves = [move for move in board.legal_moves]
+print("What are all the legal moves now?\n", moves)
+print(f"The next first legal move is from {moves[0].from_square} to {moves[0].to_square}, also known as {chess.square_name(moves[0].from_square)} and {chess.square_name(moves[0].to_square)}")
+print("So, if we know what piece we'd like to move next, we can gather all legal moves for that piece (like this black knight):", [move for move in board.legal_moves if move.from_square == 62])
